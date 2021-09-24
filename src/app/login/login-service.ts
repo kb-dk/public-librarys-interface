8a10ac32af8d7f902d32246bd2de15089402a868
zahra <zahramousavidk@gmail.com>
2021-09-24 02:09:29 -0700

    Fixed encoding and content-type for login response

---
+++ src/app/login/login-service.ts
@@ -0,0 +1,38 @@
+import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, throwError } from "rxjs";
+import { catchError, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http: HttpClient) { }
+
+  validate(libraryNumber:string, postcode:string) {
+    let loginUrl = 'http://devel12.statsbiblioteket.dk:9011/librarylending/v1/checkCreds?libraryNumber='+libraryNumber+'&postcode='+postcode;
+    const headers = new HttpHeaders({'Content-Type':'text/plain; charset=utf-8'});
+    return this.http.get(loginUrl, {headers: headers, responseType: 'text'}).pipe(
+      tap(data => console.log('All: ', typeof data)),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(err: HttpErrorResponse): Observable<never> {
+    // in a real world app, we may send the server to some remote logging infrastructure
+    // instead of just logging it to the console
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
+
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {LoginService} from "./login-service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'login',
@@ -9,8 +11,22 @@ export class LoginComponent implements OnInit {
 
   partnerCode: string = '';
   password: string = '';
+  errorMessage: string = '';
 
-  constructor() { }
+  constructor(private loginService :LoginService, private router :Router) { }
+
+  checkValidity(){
+    console.log(this.partnerCode, this.password);
+    this.loginService.validate(this.partnerCode, this.password).subscribe({
+      next: libraryName => {
+        // send params and naviate to a new page
+        console.log(libraryName);
+        this.router.navigate(['loans', {partnerCode: this.partnerCode, libraryName: libraryName}]);
+      },
+      error: err => this.errorMessage = err
+    });
+
+  }
 
   ngOnInit(): void {
   }
