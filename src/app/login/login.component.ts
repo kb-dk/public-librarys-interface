b275323eeb9d50e8a19f6e016f08cead8237416a
zahra <zahramousavidk@gmail.com>
2021-09-24 07:17:57 -0700

    Handled errors

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,17 +12,24 @@ export class LoginComponent implements OnInit {
   partnerCode: string = '';
   password: string = '';
   errorMessage: string = '';
+  loginError: boolean = false;
 
   constructor(private loginService :LoginService, private router :Router) { }
 
   checkValidity(){
-    this.loginService.validate(this.partnerCode, this.password).subscribe({
-      next: libraryName => {
-        // send params and naviate to a new page
-        this.router.navigate(['loans', {partnerCode: this.partnerCode, libraryName: libraryName}]);
-      },
-      error: err => this.errorMessage = err
-    });
+    if(parseInt(this.partnerCode)>=100000 && parseInt(this.partnerCode)<=999999 && parseInt(this.password)>=1000 && parseInt(this.password)<=9999) {
+      this.loginService.validate(this.partnerCode, this.password).subscribe({
+        next: libraryName => {
+          this.router.navigate(['loans', {partnerCode: this.partnerCode, libraryName: libraryName}]);
+        },
+        error: err => {
+          this.errorMessage = err;
+          this.loginError = true;
+        }
+      });
+    } else {
+      this.loginError = true;
+    }
 
   }
 
