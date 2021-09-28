4f00cac39a0950433f8e686ed65bc832c6886fd1
zahra <zahramousavidk@gmail.com>
2021-09-28 03:52:23 -0700

    Created header component and moved header and footer into shared folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,16 @@ import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {LoginComponent} from './login/login.component';
 import {RouterModule} from "@angular/router";
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoansComponent,
     LoginComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
