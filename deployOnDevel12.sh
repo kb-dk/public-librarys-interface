b43b9368df64c8f6b0842da492d62aeea3232266
Asger Askov Blekinge <abr@kb.dk>
2021-11-09 11:53:22 +0100

    Devel12 deployment script

---
+++ deployOnDevel12.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(dirname "$(readlink -f -- "${BASH_SOURCE[0]}")")
+
+set -e
+
+#port range 9010-9019
+#9010: Tomcat shutdown
+#9011: Tomcat http
+#9019: tomcat debug
+
+develServer=devel12.statsbiblioteket.dk
+user="almalile" #ALMA LIbrary LEnding
+devel="$user@${develServer}"
+projectName=$(basename "$SCRIPT_DIR")
+tomcatHttpPort=9011
+tomcatDebugPort=9019
+
+version=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version -Psbprojects-nexus | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }')
+
+#Build
+#(cd "$SCRIPT_DIR/"..; pwd; mvn $1 package -Psbprojects-nexus -DskipTests=true --also-make --projects "$(basename "$SCRIPT_DIR")") || exit 1
+
+#install
+rsync -av "$SCRIPT_DIR/target/$projectName-${version}.war" "${devel}:services/tomcat-apps/$projectName.war"
+rsync -av "$SCRIPT_DIR/conf" --exclude='*-local.yaml' --exclude "/**/**/" "${devel}:services/"
+rsync -av "$SCRIPT_DIR/conf/devel12/" "${devel}:services/conf/"
+rsync -av "$SCRIPT_DIR/conf/devel12/${projectName}.xml" "${devel}:tomcat/conf/Catalina/localhost/${projectName}.xml"
+
+echo "Stopping tomcat"
+ssh "$devel"  "(source .bash_profile && ~/bin/\$USER-tomcat.sh stop | grep 'tomcat is not running') || sleep 10"
+echo "Tomcat stopped"
+
+echo "Starting tomcat"
+ssh "$devel" "(source .bash_profile; export JPDA_ADDRESS='0.0.0.0:$tomcatDebugPort'; ~/bin/\$USER-tomcat.sh jpda start)"
+
+
+
+echo "$projectName.war deployed to $develServer:  http://$develServer:$tomcatHttpPort/$projectName"
