e5c8e5d046339f861df95e47375af4708bc69903
Asger Askov Blekinge <abr@kb.dk>
2021-12-02 12:47:31 +0100

    Correct deployment as laanestatus

--- deployOnDevel12.sh
+++ deployOnDevel12.sh
@@ -12,7 +12,8 @@ set -e
 develServer=devel12.statsbiblioteket.dk
 user="almalile" #ALMA LIbrary LEnding
 devel="$user@${develServer}"
-projectName=laanestatus
+projectBaseUrl=laanestatus
+projectName=$(basename "$SCRIPT_DIR")
 tomcatHttpPort=9011
 tomcatDebugPort=9019
 
@@ -22,18 +23,18 @@ version=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpres
 (cd "$SCRIPT_DIR/"..; pwd; mvn $1 package -Psbprojects-nexus -DskipTests=true --also-make --projects "$(basename "$SCRIPT_DIR")") || exit 1
 
 #install
-rsync -av "$SCRIPT_DIR/target/$projectName-${version}.war" "${devel}:services/tomcat-apps/$projectName.war"
+rsync -av "$SCRIPT_DIR/target/${projectName}-${version}.war" "${devel}:services/tomcat-apps/${projectName}.war"
 rsync -av "$SCRIPT_DIR/conf" --exclude='*-local.yaml' --exclude "/**/**/" "${devel}:services/"
 rsync -av "$SCRIPT_DIR/conf/devel12/" "${devel}:services/conf/"
-rsync -av "$SCRIPT_DIR/conf/devel12/${projectName}.xml" "${devel}:tomcat/conf/Catalina/localhost/${projectName}.xml"
+rsync -av "$SCRIPT_DIR/conf/devel12/${projectBaseUrl}.xml" "${devel}:tomcat/conf/Catalina/localhost/${projectBaseUrl}.xml"
 
 echo "Stopping tomcat"
-ssh "$devel"  "(source .bash_profile && ~/bin/\$USER-tomcat.sh stop | grep 'tomcat is not running') || sleep 10"
+ssh "${devel}"  "(source .bash_profile && ~/bin/\$USER-tomcat.sh stop | grep 'tomcat is not running') || sleep 10"
 echo "Tomcat stopped"
 
 echo "Starting tomcat"
-ssh "$devel" "(source .bash_profile; export JPDA_ADDRESS='0.0.0.0:$tomcatDebugPort'; ~/bin/\$USER-tomcat.sh jpda start)"
+ssh "${devel}" "(source .bash_profile; export JPDA_ADDRESS='0.0.0.0:${tomcatDebugPort}'; ~/bin/\$USER-tomcat.sh jpda start)"
 
 
 
-echo "$projectName.war deployed to $develServer:  http://$develServer:$tomcatHttpPort/$projectName"
+echo "${projectName}.war deployed to ${develServer}:  http://${develServer}:${tomcatHttpPort}/${projectBaseUrl}"
--- installLocally.sh
+++ installLocally.sh
@@ -6,14 +6,15 @@ set -e
 
 pushd $SCRIPT_DIR > /dev/null
 
-port=4200
+port=4210
 shutdownPort=4206
 debugPort=4276
-appName=laanestatus
+projectBaseUrl=laanestatus
+projectName=$(basename "$SCRIPT_DIR")
 
 version=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version -Psbprojects-nexus | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }')
 
-#(mvn $1 clean package -Psbprojects-nexus -DskipTests=true) || exit 1
+(mvn $1 package -Psbprojects-nexus -DskipTests=true) || exit 1
 
 TOMCAT_VERSION=9.0.36
 #TOMCAT_VERSION=8.5.51
@@ -22,8 +23,8 @@ wget -N "https://archive.apache.org/dist/tomcat/tomcat-$(echo $TOMCAT_VERSION | 
 
 TOMCAT_HOME="$SCRIPT_DIR/apache-tomcat-$TOMCAT_VERSION"
 
-sed -i "s/<Connector port=\"8080\"/<Connector port=\"$port\"/g" "$TOMCAT_HOME/conf/server.xml"
-sed -i "s/<Server port=\"8005\" shutdown=\"SHUTDOWN\">/<Server port=\"$shutdownPort\" shutdown=\"SHUTDOWN\">/g" "$TOMCAT_HOME/conf/server.xml"
+sed -i "s/<Connector port=\"\d+'\"/<Connector port=\"$port\"/g" "$TOMCAT_HOME/conf/server.xml"
+sed -i "s/<Server port=\"\d+\" shutdown=\"SHUTDOWN\">/<Server port=\"$shutdownPort\" shutdown=\"SHUTDOWN\">/g" "$TOMCAT_HOME/conf/server.xml"
 
 "$TOMCAT_HOME"/bin/shutdown.sh
 
@@ -31,7 +32,7 @@ rm -rf "$TOMCAT_HOME/webapps/"*
 rm -rf "$TOMCAT_HOME/logs/"*
 
 mkdir -p "$TOMCAT_HOME/conf/Catalina/localhost/"
-cp -f "$SCRIPT_DIR/conf/local/$appName.xml" "$TOMCAT_HOME/conf/Catalina/localhost/$appName.xml"
+cp -f "$SCRIPT_DIR/conf/local/${projectBaseUrl}.xml" "$TOMCAT_HOME/conf/Catalina/localhost/${projectBaseUrl}.xml"
 
 sleep 5
 
