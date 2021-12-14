cd5aebf307e94d7d3f219e713757700c96049ff7
Asger Askov Blekinge <abr@kb.dk>
2021-12-14 13:52:45 +0100

    Development uses https backend server

--- laanestatus_node_frontend/conf/environment.development.ts
+++ laanestatus_node_frontend/conf/environment.development.ts
@@ -1,9 +1,5 @@
 export const environment = {
   production: false,
-  CHECK_CREDS_URL: '/librarylending/v1/checkCreds',
-  GET_LOANS_URL: '/librarylending/v1/partnerLoans/',
-
-    // CHECK_CREDS_URL: 'http://localhost:9011/librarylending/v1/checkCreds',
-    // GET_LOANS_URL: 'http://localhost:9011/librarylending/v1/partnerLoans/',
-
+  CHECK_CREDS_URL: 'https://devel12.statsbiblioteket.dk/librarylending/v1/checkCreds',
+  GET_LOANS_URL: 'https://devel12.statsbiblioteket.dk/librarylending/v1/partnerLoans/',
 };
