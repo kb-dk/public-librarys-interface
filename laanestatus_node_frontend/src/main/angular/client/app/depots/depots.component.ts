0e1c98d508e65b6bcf53269a43b307984872868d
zahra <zahramousavidk@gmail.com>
2022-01-04 04:51:46 -0800

    Cleaned up

--- laanestatus_node_frontend/src/main/angular/client/app/depots/depots.component.ts
+++ laanestatus_node_frontend/src/main/angular/client/app/depots/depots.component.ts
@@ -25,21 +25,10 @@ export class DepotsComponent implements OnInit {
 
             },
             () => {
-                console.groupCollapsed('Testing console functionalities:'); // Categorize the following console commands in a collapsable group
-                console.table(_depots);
-                console.dir(_depots);
-                console.log(_depots);
-                console.assert(depots.length > 0, 'problem in fetching data'); // print the second argument if the assertion is false, otherwise shows nothing
-                console.groupEnd();
-
                 console.error('Error fetching data, using hardcoded data instead.');
                 this.depots = _depots;
             }
         );
-
-    }
-
-    sortArrays() {
 
     }
 
