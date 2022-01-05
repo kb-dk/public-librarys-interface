3c863d6ac902e80cd8f4bf69e114a0b29d4f3019
Asger Askov Blekinge <abr@kb.dk>
2022-01-05 14:40:22 +0100

    new environment.development.ts

--- laanestatus_node_frontend/conf/environment.development.ts
+++ laanestatus_node_frontend/conf/environment.development.ts
@@ -1,12 +1,15 @@
 export const environment = {
     production: false,
+
+    API_HOST_URL: 'http://localhost:4220/laanestatus/',
+
     //Things ending with / expects to code to postfix an ID
     //things NOT ending in / expects to be used directly
-    CHECK_CREDS_URL: 'http://localhost:4220/laanestatus/api/checkCreds',
-    GET_LOANS_URL: 'http://localhost:4220/laanestatus/api/partnerLoans/',
+    CHECK_CREDS_URL: 'api/checkCreds',
+    GET_LOANS_URL: 'api/partnerLoans/',
 
     //BCI DEPOTS
-    GET_DEPOTS_URL: 'http://localhost:4220/laanestatus/api/depots', //{partnerID}
-    GET_DEPOT_URL: 'http://localhost:4220/laanestatus/api/depot/', //{partnerID}/{depotID}
-    GET_DEPOT_PDF_URL: 'http://localhost:4220/laanestatus/api/depot/pdf/', //{partnerID}/{depotID}/{type}
+    GET_DEPOTS_URL: 'api/depots', //{partnerID}
+    GET_DEPOT_URL: 'api/depot/', //{partnerID}/{depotID}
+    GET_DEPOT_PDF_URL: 'api/depot/pdf/', //{partnerID}/{depotID}/{type}
 };
