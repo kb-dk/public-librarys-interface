3a3abde965c79bc5ea38c34ec4fa050e54bbd1f2
Asger Askov Blekinge <abr@kb.dk>
2022-01-05 11:07:13 +0100

    interlibraryloans can run a local angular in order for Zahra development

--- laanestatus_node_frontend/conf/environment.development.ts
+++ laanestatus_node_frontend/conf/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    CHECK_CREDS_URL: 'api/checkCreds',
-    GET_LOANS_URL: 'api/partnerLoans/',
+    CHECK_CREDS_URL: 'http://localhost:4220/laanestatus/api/checkCreds',
+    GET_LOANS_URL: 'http://localhost:4220/laanestatus/api/partnerLoans/',
 };
