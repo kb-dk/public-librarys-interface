da906b95eebe865ceb49885cd3bd8a1c07fdf447
Asger Askov Blekinge <abr@kb.dk>
2022-01-05 13:50:46 +0100

    environment list all api methods

--- laanestatus_node_frontend/conf/environment.production.ts
+++ laanestatus_node_frontend/conf/environment.production.ts
@@ -1,5 +1,13 @@
 export const environment = {
     production: true,
+
+    //Things ending with / expects to code to postfix an ID
+    //things NOT ending in / expects to be used directly
     CHECK_CREDS_URL: 'api/checkCreds',
     GET_LOANS_URL: 'api/partnerLoans/',
+
+    //BCI DEPOTS
+    GET_DEPOTS_URL: 'api/depots', //{partnerID}
+    GET_DEPOT_URL: 'api/depot/', //{partnerID}/{depotID}
+    GET_DEPOT_PDF_URL: 'api/depot/pdf/', //{partnerID}/{depotID}/{type}
 };
--- laanestatus_node_frontend/src/main/angular/client/environments/environment.ts
+++ laanestatus_node_frontend/src/main/angular/client/environments/environment.ts
@@ -4,8 +4,17 @@
 
 export const environment = {
   production: false,
-  CHECK_CREDS_URL: 'http://devel12.statsbiblioteket.dk:9011/librarylending/v1/checkCreds',
-  GET_LOANS_URL: 'http://devel12.statsbiblioteket.dk:9011/librarylending/v1/partnerLoans/',
+    //Things ending with / expects to code to postfix an ID
+    //things NOT ending in / expects to be used directly
+    CHECK_CREDS_URL: 'api/checkCreds',
+    GET_LOANS_URL: 'api/partnerLoans/',
+
+    //BCI DEPOTS
+    GET_DEPOTS_URL: 'api/depots', //{partnerID}
+    GET_DEPOT_URL: 'api/depot/', //{partnerID}/{depotID}
+    GET_DEPOT_PDF_URL: 'api/depot/pdf/', //{partnerID}/{depotID}/{type}
+
+    API_HOST_URL: '',
 };
 
 /*
