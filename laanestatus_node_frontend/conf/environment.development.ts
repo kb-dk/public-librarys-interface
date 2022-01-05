d00939ded72e179098d290bd86d31ff0bdcc9630
Asger Askov Blekinge <abr@kb.dk>
2022-01-05 11:22:57 +0100

    bci depots sample URLS added to development config

--- laanestatus_node_frontend/conf/environment.development.ts
+++ laanestatus_node_frontend/conf/environment.development.ts
@@ -1,5 +1,12 @@
 export const environment = {
     production: false,
+    //Things ending with / expects to code to postfix an ID
+    //things NOT ending in / expects to be used directly
     CHECK_CREDS_URL: 'http://localhost:4220/laanestatus/api/checkCreds',
     GET_LOANS_URL: 'http://localhost:4220/laanestatus/api/partnerLoans/',
+
+    //BCI DEPOTS
+    GET_DEPOTS_URL: 'http://localhost:4220/laanestatus/api/depots',
+    GET_DEPOT_URL: 'http://localhost:4220/laanestatus/api/depot/',
+    GET_DEPOT_PDF_URL: 'http://localhost:4220/laanestatus/api/depot/pdf/',
 };
