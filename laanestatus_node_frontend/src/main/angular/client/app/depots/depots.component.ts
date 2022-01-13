5a76231f7dead7813d4c00b49da3371dae0ff671
zahra <zahramousavidk@gmail.com>
2022-01-13 06:23:35 -0800

    Added sorting to depots

--- laanestatus_node_frontend/src/main/angular/client/app/depots/depots.component.ts
+++ laanestatus_node_frontend/src/main/angular/client/app/depots/depots.component.ts
@@ -12,7 +12,7 @@ import {environment} from '../../environments/environment';
     styleUrls: ['./depots.component.scss']
 })
 export class DepotsComponent implements OnInit {
-    @Input() depotsInfo!:IDepotsAndLibraryInfo;
+    @Input() depotsInfo!: IDepotsAndLibraryInfo;
     depots!: IDepot[];
     $depotEntries!: Observable<IDepotEntry[]>;
     depotEntryNr!: number;
@@ -40,5 +40,42 @@ export class DepotsComponent implements OnInit {
 
     printDepot() {
         window.print();
+    }
+
+    sortArrays(field: keyof IDepot, isDate: boolean) {
+        let sortingOrder: string = DepotsComponent.getCurrentSortingOrder(field, isDate) === 'desc' ? 'asc' : 'desc';
+        DepotsComponent.addAndRemoveSortClasses(field, sortingOrder);
+
+        [this.depots] = [this.depots].map(array => this.sort(array.slice(0), field, isDate, sortingOrder));
+    }
+
+    static getCurrentSortingOrder(field: keyof IDepot, isDate: boolean): string {
+        if(isDate){
+            return document.getElementById(field)!.classList.contains('desc') ? 'desc' : 'asc'; // Default Descending for dates
+        } else {
+            return document.getElementById(field)!.classList.contains('asc') ? 'asc' : 'desc'; // Default ascending for text
+        }
+    }
+
+    static addAndRemoveSortClasses(field: keyof IDepot, className: string): void {
+        document.querySelectorAll('.sortable').forEach((el) => el.classList.remove('desc', 'asc'));
+        document.getElementById(field)!.classList.add(className);
+    }
+
+    sort(array: any[], field: keyof IDepot, isDate: boolean, sortingOrder: string) {
+        if (isDate) {
+            if (sortingOrder === 'desc') {
+                array.sort((a, b) => +new Date(b[field]) - +new Date(a[field]))
+            } else {
+                array.sort((a, b) => +new Date(a[field]) - +new Date(b[field]))
+            }
+        } else {
+            if (sortingOrder === 'desc') {
+                array.sort((a, b) => b[field] > a[field] ? 1 : -1)
+            } else {
+                array.sort((a, b) => a[field] > b[field] ? 1 : -1)
+            }
+        }
+        return array;
     }
 }
